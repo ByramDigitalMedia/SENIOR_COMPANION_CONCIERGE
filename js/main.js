06b6c8b2fbbe25aaa09352d4385649d6634e9464
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-07-30 15:52:05 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* =======================================================
+  
+  * Template URL: https://byramdigitalmedia.com
+  * Updated: July 30 2025  
+  * Author: BDM
+  * License: MIT
+  ======================================================== */
+
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
